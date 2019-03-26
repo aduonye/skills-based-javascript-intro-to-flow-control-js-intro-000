951ee71e8c26552f9c3ce3b163d6df31a0214de0
aduonye iwarimie <aduonyeiwarimie@gmail.com>
2019-03-26 19:05:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,5 +15,11 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+  switch(age) {
+    case 13 >= 19 :
+    return "You are a teenager"
+    default:
+    return "You have an age"
+  }
+console.log("How old are you?")
 }
